de319d6e4fd0e92cb4cd8e4da006a7216d9d2c89
vad1m24 <vadim.rutkevich@gmail.com>
2022-10-14 11:11:29 +0300

    Lesson2_py

--- Lesson2/Task1.py
+++ Lesson2/Task1.py
@@ -1,10 +1,10 @@
 # Напишите программу, которая принимает на вход вещественное или целое число и показывает сумму его цифр.
 # Через строку нельзя решать.
 
-number = float(input('Введите число: '))
+number = float(input('Введите число: \n'))
 number = number * 10 ** 10
 sum_num = 0
 while number > 0:
     sum_num = sum_num + number % 10
     number = number // 10
-print(sum_num)
+print(int(sum_num))
--- Lesson2/Task3.py
+++ Lesson2/Task3.py
@@ -5,7 +5,7 @@
 text1 = input('Введите предложение 1: \n')
 text2 = input('Введите предложение 2: \n')
 count = 0
-for duplicate in range(len(text2) - len(text1) + 1):
+for duplicate in range(len(text1) - len(text2) + 1):
     if text1[duplicate:duplicate + len(text2)] == text2:
         count = count + 1
 print(count)
